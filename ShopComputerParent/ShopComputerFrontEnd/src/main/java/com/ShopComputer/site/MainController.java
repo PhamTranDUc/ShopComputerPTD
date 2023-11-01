61e01ccde9a839ac1290d4cd9fe88898fce67901
phamtranduc <phamtranducc@gmail.com>
2023-11-01 21:50:59 +0700

    index frontEnd

--- ShopComputerParent/ShopComputerFrontEnd/src/main/java/com/ShopComputer/site/MainController.java
+++ ShopComputerParent/ShopComputerFrontEnd/src/main/java/com/ShopComputer/site/MainController.java
@@ -1,13 +1,27 @@
 package com.ShopComputer.site;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ShopComputer.site.brand.BrandRepository;
+import com.ShopComputer.site.category.CategoryRepository;
 
 @Controller
 public class MainController {
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private BrandRepository brandRepository;
+	
+	
 	
 	@GetMapping("")
-	public String viewHomePage() {
+	public String viewHomePage(Model model) {
+		model.addAttribute("listCategory",categoryRepository.findAllEnable());
+		model.addAttribute("listBrand", brandRepository.findAll());
 		return "index";
 	}
 	
--- ShopComputerParent/ShopComputerFrontEnd/src/main/java/com/ShopComputer/site/ShopComputerFrontEndApplication.java
+++ ShopComputerParent/ShopComputerFrontEnd/src/main/java/com/ShopComputer/site/ShopComputerFrontEndApplication.java
@@ -2,8 +2,10 @@ package com.ShopComputer.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.ShopComputer.EntityCommon"})
 public class ShopComputerFrontEndApplication {
 
 	public static void main(String[] args) {
